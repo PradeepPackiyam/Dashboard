1da2aaf4eb5219d3d3fdbcf14332c1360704a1ec
PradeepPackiyam <110380081+PradeepPackiyam@users.noreply.github.com>
2022-08-23 16:13:44 +0530

    product-list-error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './Components/Home';
 import Login from './Components/Login';
 import Dashboardpage from './Components/Dashboardpage';
 
-import DashboardProductlist from './Components/DashboardProductlist';
+
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route exact path='/' element={<Home />}></Route>
           <Route exact path='/login' element={<Login />}></Route>
           <Route exact path='/dashboardpage' element={<Dashboardpage />}></Route>
-          <Route exact path='/dashboardProductlist' element={<DashboardProductlist />}></Route>
+          
         </Routes>
 
 
